d308f33769f5922ba0ccf5daf4c2049cbbdbff68
rstraaa-04 <ygyandre9@gmail.com>
2025-09-06 23:26:54 +0700

    Update script.js

--- hbd/script.js
+++ hbd/script.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     cover.addEventListener('click', () => {
         cover.classList.add('open');
 
-        let delay = 1000; // Jeda 1 detik setelah sampul terbuka
+        let delay = 8000; // Jeda 1 detik setelah sampul terbuka
 
         pages.forEach((page, index) => {
             setTimeout(() => {
                 page.style.transform = `rotateY(-180deg)`;
                 // Mengatur z-index agar halaman di belakang maju ke depan
                 page.style.zIndex = pages.length - index;
-            }, delay + (index * 1000)); // Halaman akan membalik dengan jeda 1 detik
+            }, delay + (index * 8000)); // Halaman akan membalik dengan jeda 1 detik
         });
     });
 });
